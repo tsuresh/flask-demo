3f7be412d620594fa68ff14810af495027de1f97
Suresh Peiris <tsmpmail@gmail.com>
2024-06-13 13:59:38 +0530

    Added mysql file

--- starter.py
+++ starter.py
@@ -4,9 +4,14 @@ app = Flask(__name__)
 
 @app.route('/items', methods=['GET'])
 def read_items():
-    """Read all items."""
     return jsonify({
         'message': 'Hello, World!'
+    }), 200
+
+@app.route('/post', methods=['POST'])
+def post_items():
+    return jsonify({
+        'message': 'This is a post request!'
     }), 200
 
 if __name__ == '__main__':
